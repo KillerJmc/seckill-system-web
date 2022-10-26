b19d858c927cf0aeb89375d12cd886c3f332b222
Jmc <1934497212@qq.com>
2022-10-26 17:31:51 +0800

    Add dist branch.

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import App from "@/App.vue"
-import router from "@/router"
-import { createApp } from "vue"
-import { createPinia } from "pinia"
-
-createApp(App)
-    .use(router)
-    .use(createPinia())
-    .mount("#app")
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite"
-import type { UserConfig } from "vite"
-import DevConfig from "./vite.config.dev"
-import ProdConfig from "./vite.config.prod"
-
-export default defineConfig(
-    config => <UserConfig> (config.command === "serve" ? DevConfig : ProdConfig)
-)
