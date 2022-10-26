dfff7b9d554bd8b739edd8c85a54a383b2748ca0
Jmc <1934497212@qq.com>
2022-10-26 17:23:24 +0800

    1. Update version tag. 2. Update cdn config.

--- vite.config.prod.ts
+++ vite.config.prod.ts
@@ -8,12 +8,12 @@ export default defineConfig({
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
             // CDN加速
-            "vue": "https://esm.sh/vue@3.2.41",
-            "vue-router": "https://esm.sh/vue-router@4.1.6",
-            "axios": "https://esm.sh/axios@1.1.2",
-            "pinia": "https://esm.sh/pinia@2.0.23",
-            "js-cookie": "https://esm.sh/js-cookie@3.0.1",
-            "js-sha256": "https://esm.run/js-sha256@0.9.0"
+            "vue": "https://esm.sh/vue@^3.2.41",
+            "vue-router": "https://esm.sh/vue-router@^4.1.6",
+            "axios": "https://esm.sh/axios@^1.1.2",
+            "pinia": "https://esm.sh/pinia@^2.0.23",
+            "js-cookie": "https://esm.sh/js-cookie@^3.0.1",
+            "js-sha256": "https://esm.run/js-sha256@^0.9.0"
         }
     },
     server: {
