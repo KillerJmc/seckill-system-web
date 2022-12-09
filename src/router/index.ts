5c045f087f8ec77d393a6ad46f097209c1df18e5
Jmc <1934497212@qq.com>
2022-12-10 01:52:03 +0800

    Github pages.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter } from "vue-router"
+import { createWebHashHistory } from "vue-router"
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {
             path: "/",
